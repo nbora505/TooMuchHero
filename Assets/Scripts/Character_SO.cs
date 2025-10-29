793f2a5ab155fbdf3e0d1a7321ac1722887fb1c6
nbora505 <manhasum@naver.com>
2025-10-29 17:54:31 +0900

    스크립터블오브젝트

---
+++ Assets/Scripts/Character_SO.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+[CreateAssetMenu]
+public class Character_SO : ScriptableObject
+{
+    public int id;
+    public string characterName;
+    public int atk;
+    public int hp;
+    public int star;
+    public int exp;
+
+    public enum When { turnStart, battleStart, dead, selled, bought, frontAttack, damaged, lose }
+    public enum What { getHp, getAtk, getBoth, getBuff, extraAtk, summon, getGold, getExp }
+    public enum Target { self, randomEnemy, randomFriend, range, backFriend, frontFriend, firstEnemy, firstFriend }
+
+    public When when;
+    public What what;
+    public Target target;
+
+    public int[] skillValue = new int[3];
+    public int[] targetValue = new int[3];
+
+    [TextArea]
+    public string[] skillDescription = new string[3];
+    public Sprite characterSprite;
+}
